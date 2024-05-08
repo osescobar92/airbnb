a79c014ecc11f2a9c042c50fd189b4201e5bb890
Oscar Escobar Ulloa <osescobar92@gmail.com>
2024-05-07 22:24:14 -0400

    Create Reservations CRUD
    - Use the abstract repository in reservations repository.

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
-import { DatabaseModule } from '@app/common'
+import { ReservationsController } from './reservations.controller';
+import { DatabaseModule } from '@app/common';
+import { ReservationsRepository } from './reservations.repository';
+import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [ReservationsController],
-  providers: [ReservationsService],
+    imports: [
+        DatabaseModule, 
+        DatabaseModule.forFeature([{ 
+            name: ReservationDocument.name, 
+            schema: ReservationSchema 
+        }])
+    ],
+    controllers: [ReservationsController],
+    providers: [ReservationsService, ReservationsRepository]
 })
 export class ReservationsModule {}
