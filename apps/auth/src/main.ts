a693d021a39869a98a350cb871c80ed73ad6e460
Oscar Escobar Ulloa <osescobar92@gmail.com>
2024-05-16 13:35:22 -0400

    Import Logger module in auth module

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
-import { Logger as PinoLogger } from 'nestjs-pino';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.useLogger(app.get(PinoLogger));
+  app.useLogger(app.get(Logger));
   await app.listen(3001);
 }
 bootstrap();
