b92a3f589b756e2fc6dbb8f1930682859cd3c154
Oscar Escobar Ulloa <osescobar92@gmail.com>
2024-05-27 14:55:51 -0400

    Refactorize the config module
     - Delete the config modules in the library/common
     - Use the config module of nest
     - Add a env file to reservations and database module

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,7 +4,8 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
-
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
     imports: [
@@ -13,7 +14,13 @@ import { ReservationDocument, ReservationSchema } from './models/reservation.sch
             name: ReservationDocument.name, 
             schema: ReservationSchema 
         }]),
-        LoggerModule
+        LoggerModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validationSchema: Joi.object({
+                MONGODB_URI: Joi.string().required(),
+            })
+        })
     ],
     controllers: [ReservationsController],
     providers: [ReservationsService, ReservationsRepository]
