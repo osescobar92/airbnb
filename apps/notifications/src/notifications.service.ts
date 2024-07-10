4f39b7e907d95fab978592a8e648831d778e407a
Oscar Escobar Ulloa <osescobar92@gmail.com>
2024-07-10 12:33:26 -0400

    Install and configure nodemailer

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { NotifyEmailDto } from './dto/notify-email.dto';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class NotificationsService {
 
-    async notifyEmail({email}: NotifyEmailDto) {
-        console.log(email);
+    async notifyEmail({ email, text}: NotifyEmailDto) {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: 'adrianna.mraz@ethereal.email',
+                pass: 'MUxK4RtKSR3BZjYcQ2'
+            }
+        });
+
+        await transporter.sendMail({
+            from: 'adrianna.mraz@ethereal.email',
+            to: email,
+            subject: 'Sleepr Notification',
+            text,
+        });
     }
 
 }
--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -31,7 +31,10 @@ export class PaymentsService {
             }
         });
 
-        this.notificationsService.emit('notify_email', {email})
+        this.notificationsService.emit('notify_email', {
+            email,
+            text: `Your payment of $${amount} has completed successfully.`
+        })
 
         return paymentsIntents;
     }
