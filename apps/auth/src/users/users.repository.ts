941c3ef6d2551a6bc3d048b3e9a160dda0cdcd8d
Oscar Escobar Ulloa <osescobar92@gmail.com>
2024-08-06 17:05:33 -0400

    Productionize reservations dockerfile
    Productionize the reservations dockerfile and move the user schema from
    the users module to the libs common folder.

--- apps/auth/src/users/users.repository.ts
+++ apps/auth/src/users/users.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from "@nestjs/common";
-import { UserDocument } from "./models/user.schema";
+import { UserDocument } from "@app/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { AbstractRepository } from "@app/common";
